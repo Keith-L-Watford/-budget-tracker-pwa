889eb0ad00c92ee11371b932794cb3187703acbc
Keith Watford <keith.l.watford@gmail.com>
2021-07-29 18:52:38 -0400

    added the file paths for files_to_cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,12 @@
 console.log("Hello from service worker!")
 
 const FILES_TO_CACHE = [
-    //  the files
+    // just '/filename' because of line 16 in server.js - everything to the public folder is set up
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    '/index.html',
+    '/manifest.webmanifest',
+    '/styles.css',
 ];
 
 
